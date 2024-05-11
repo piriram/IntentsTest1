ec39a97efb3fdd827ecf5a801a3f48da23b5339c
piri <pyoram25@gmail.com>
2024-05-11 19:46:16 +0900

    Initial Commit

---
+++ IntentsTest1/IntentsTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  IntentsTest1App.swift
+//  IntentsTest1
+//
+//  Created by ram on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct IntentsTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
